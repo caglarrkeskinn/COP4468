44528bc0adaa3b0712f816e749f99e71ef77c5a4
Çağlar Keskin <caglarkeskin@Caglar-MacBook-Pro.local>
2023-06-06 22:17:08 +0300

    delete and add to fav functionalities are added

--- screens/ProductScreen.tsx
+++ screens/ProductScreen.tsx
@@ -9,20 +9,43 @@ import {
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+interface Product {
+  id: any;
+  name: string;
+  isFavorite: boolean;
+}
+
 const ProductScreen = () => {
-  const [products, setproducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     axios.get('https://northwind.vercel.app/api/products').then(res => {
-      setproducts(res.data);
+      setProducts(res.data);
     });
   }, []);
+  const handleDelete = (productId: any) => {
+    const updatedProducts = products.filter(item => item.id !== productId);
+    setProducts(updatedProducts);
+  };
+
+  const handleFavorite = (productId: any) => {
+    const updatedProducts = products.map(item => {
+      if (item.id === productId) {
+        return {
+          ...item,
+          isFavorite: !item.isFavorite,
+        };
+      }
+      return item;
+    });
+    setProducts(updatedProducts);
+  };
 
   return (
     <SafeAreaView>
       <FlatList
         data={products}
-        renderItem={({item}: any) => (
+        renderItem={({item}: {item: Product}) => (
           <View style={{flexDirection: 'row'}}>
             <TouchableOpacity>
               <Text
@@ -33,42 +56,20 @@ const ProductScreen = () => {
                 {item.name}
               </Text>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => handleDelete(item.id)}>
               <Text style={{color: 'tomato'}}>Delete</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-              <Text style={{color: 'blue'}}>Fav</Text>
+
+            <TouchableOpacity onPress={() => handleFavorite(item.id)}>
+              <Text style={{color: item.isFavorite ? 'blue' : 'black'}}>
+                Favorite
+              </Text>
             </TouchableOpacity>
           </View>
         )}
+        keyExtractor={(item: Product) => item.id.toString()}
       />
     </SafeAreaView>
-
-    // <SafeAreaView>
-    //   <View style={{flexDirection: 'row'}}>
-    //     <TouchableOpacity>
-    //       <Text>Prdouct 1</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity>
-    //       <Text style={{color: 'tomato'}}>Delete</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity>
-    //       <Text style={{color: 'blue'}}>Add to Fav</Text>
-    //     </TouchableOpacity>
-    //   </View>
-
-    //   <View style={{flexDirection: 'row'}}>
-    //     <TouchableOpacity>
-    //       <Text>Prdouct 2</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity>
-    //       <Text style={{color: 'tomato'}}>Delete</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity>
-    //       <Text style={{color: 'blue'}}>Add to Fav</Text>
-    //     </TouchableOpacity>
-    //   </View>
-    // </SafeAreaView>
   );
 };
 
