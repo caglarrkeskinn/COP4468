15a65c5b141bc1ce462813f04f7ec90ae2159cec
aliberkkazan <aliberk.122@gmail.com>
2023-06-07 22:26:50 +0300

    Some Category Changes

--- navigator/index.tsx
+++ navigator/index.tsx
@@ -1,8 +1,8 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import ProductScreen from '../screens/ProductScreen';
-import Categories from '../screens/Categories';
+import ProductScreen from '../screens/ProductScreens/ProductScreen';
+import Categories from '../screens/CategoryScreens/CategoryList';
 import Orders from '../screens/Orders';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
